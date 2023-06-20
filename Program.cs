4516d11a43c64dd01f51608048a16be5e2e94da1
skafula <79100340+skafula@users.noreply.github.com>
2023-06-20 11:10:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void MinMaxValue(int[] arr, out int max, out int min)
+    {
+        min = arr[0];
+        max = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        var array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        MinMaxValue(array, out int max, out int min);
+        Console.WriteLine($"Minimum {min}, maximum {max}");
+    }
+}
